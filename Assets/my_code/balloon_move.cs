5ce5e2662184709847f8e57d8293c049a07609b1
A. Binzxxxxxx <DEvil0000@users.noreply.github.com>
2019-04-25 00:48:48 +0200

    added balloon rotation (physics plus scripted back rotation) and changed balloon physics

--- Assets/my_code/balloon_move.cs
+++ Assets/my_code/balloon_move.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 
 public class balloon_move : MonoBehaviour
 {
+    public float backRotationSpeed;
+
     private static System.Random rnd = new System.Random();
     private Animator animator;
     private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
 
         foreach(GameObject wallofdeath in GameObject.FindGameObjectsWithTag("wallofdeath"))
             Physics2D.IgnoreCollision(wallofdeath.GetComponent<Collider2D>(), GetComponent<Collider2D>());
@@ -24,6 +28,14 @@ public class balloon_move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.localEulerAngles.z > 180f)
+        {
+            rb.AddTorque(Time.deltaTime * backRotationSpeed * (transform.localEulerAngles.z-180f)/180f);
+        }
+        else if (transform.localEulerAngles.z > 0f)
+        {
+            rb.AddTorque(Time.deltaTime * backRotationSpeed * transform.localEulerAngles.z/180f * -1);
+        }
     }
 
     public void Pop()
@@ -39,6 +51,7 @@ public class balloon_move : MonoBehaviour
     public void Respawn()
     {
         transform.position = new Vector3((float)rnd.Next(-6, 6), -10f, -0.5f);
+        transform.localRotation = Quaternion.identity;
 
         int mainColor = rnd.Next(200, 256);
         int secondaryColor = rnd.Next(0, 106) + rnd.Next(0, 4) * 50;
@@ -47,9 +60,9 @@ public class balloon_move : MonoBehaviour
         permute(permutation);
         spriteRenderer.color = new Color(permutation[0]/255f, permutation[1]/255f, permutation[2]/255f);
 
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
         rb.velocity = Vector3.zero;
-        rb.gravityScale = (float)(rnd.Next(-10, -1))/100; //-0.01 to -0.1
+        rb.angularVelocity = 0f;
+        rb.gravityScale = (float)(rnd.Next(-5, -1))/10; //-0.1 to -0.5
 
         animator.ResetTrigger("Pop");
     }
