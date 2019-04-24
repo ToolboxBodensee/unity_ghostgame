d17a42f69cdbd62203fe6840fce0b0ff8b46aa63
A. Binzxxxxxx <DEvil0000@users.noreply.github.com>
2019-04-24 05:11:05 +0200

    even brighter colors for the balloons with some kind of random bright color algorithm

--- Assets/my_code/balloon_move.cs
+++ Assets/my_code/balloon_move.cs
@@ -40,12 +40,31 @@ public class balloon_move : MonoBehaviour
     {
         transform.position = new Vector3((float)rnd.Next(-6, 6), -10f, -0.5f);
 
-        spriteRenderer.color = new Color((float)rnd.Next(0, 255)/255, (float)rnd.Next(0, 255)/255, (float)rnd.Next(0, 255)/255);
+        int mainColor = rnd.Next(200, 256);
+        int secondaryColor = rnd.Next(0, 106) + rnd.Next(0, 4) * 50;
+        int otherColor = rnd.Next(0, 100);
+        int[] permutation = {mainColor, secondaryColor, otherColor};
+        permute(permutation);
+        spriteRenderer.color = new Color(permutation[0]/255f, permutation[1]/255f, permutation[2]/255f);
 
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
         rb.velocity = Vector3.zero;
         rb.gravityScale = (float)(rnd.Next(-10, -1))/100; //-0.01 to -0.1
 
         animator.ResetTrigger("Pop");
+    }
+
+    private void permute(int[] permutation)
+    {
+        int i;
+        for (i = 0; i < 3; i++) {
+            int j = i + rnd.Next(0, 3-i);
+            if (i != j)
+            {
+                int t = permutation[j];
+                permutation[j] = permutation[i];
+                permutation[i] = t;
+            }
+        }
     }
 }
