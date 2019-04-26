9c0a117004ae1e8f02b3f4a9c507d162b75e8169
drwuro <email@drwuro.com>
2019-04-26 23:15:25 +0200

    made the clouds move

---
+++ Assets/my_code/clouds_move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clouds_move : MonoBehaviour
+{
+    public float speed = 0.1f;
+    
+    private Material material;
+    private Vector2 offset = Vector2.zero;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+        offset = material.GetTextureOffset("_MainTex");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset.x += speed * Time.deltaTime;
+        material.SetTextureOffset("_MainTex", offset);
+    }
+}
